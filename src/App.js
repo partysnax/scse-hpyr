e3a18082152b8f710d9f4782f91900e7cd4224dd
partysnax <pat9120219@gmail.com>
2020-01-11 09:51:46 +0800

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        	Finally, this works!
+        	Hi All
         </p>
         <a
           className="App-link"
