ed48f03499fe8c22765740e161c364ea1550e587
YuerZhu <45285516+YuerZhu@users.noreply.github.com>
2020-01-11 10:05:48 +0800

    Update App.js
    yure is gry

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        	yuer
+        	yuer is gry
         </p>
         <a
           className="App-link"
