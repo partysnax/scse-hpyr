1aa7151f8a5aa69a3a432566d7f691aa5634bd21
partysnax <pat9120219@gmail.com>
2020-01-15 16:42:38 +0800

    Added search option buttons and country code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,71 @@ import React from 'react';
 import fetch from 'isomorphic-unfetch';
 import './App.css';
 const geolocator = require('geolocation');
-const PRIVATE_TOKEN = 'pk.39b2a6066afe90744c8084ecd7ba931d';
+const LOCATIONIQ_PRIVATE_TOKEN = 'pk.39b2a6066afe90744c8084ecd7ba931d';
+
+class SearchOptionButton extends React.Component {
+	handleClick = () => {
+		this.props.onClick(this.props.id);
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick} style={{display: 'block', margin: 'auto'}}>
+				{this.props.text}
+			</button>
+		);
+	}
+}
+
+class UserConfig extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			option: null,
+		}
+	}
+
+	processOption = (option) => {
+		console.log(`Option: ${option}`);
+		this.setState({
+			option: option,
+		})
+	}
+
+	render() {
+		if (this.props.lat === '') {
+			return null
+		}
+		else if (this.props.lat === 'err') {
+			return (
+				<p>Location not found!</p>
+			);
+		}
+		else {
+			const country_code = (this.props.country_code) ? this.props.country_code : 'None';
+			return (
+				<div>
+					<p>Location: {this.props.lat}, {this.props.long} ({country_code})</p>
+					<h3>Where would you like to search?</h3>
+					{(this.props.country_code) ? (<SearchOptionButton onClick={this.processOption} id={0} text="Within my country"/>) : null}
+					<SearchOptionButton onClick={this.processOption} id={1} text="Within 100 km (2 hour drive)"/>
+					<SearchOptionButton onClick={this.processOption} id={2} text="Within 3000 km (4 hour flight)"/>
+					<SearchOptionButton onClick={this.processOption} id={3} text="Anywhere!"/>
+				</div>
+			);
+		}
+	}
+}
 
 class App extends React.Component {
   constructor(props) {
   	super(props);
   	this.state = {
   		inputLocation: '',
+  		location: '',
   		locationLat: '',
 			locationLong: '',
+			locationCountry: '',
   	}
   }
 
@@ -18,12 +74,27 @@ class App extends React.Component {
 		console.log('Getting current location...');
 
 		const getPosition = (err, position) => {
-			if (err) throw err;
-			this.setState({
-				locationLat: position.coords.latitude,
-				locationLong: position.coords.longitude
-			})
-		};
+			if (err) {
+				this.setState({
+					locationLat: 'err',
+					locationLong: ''
+				})
+			}
+			else {
+				const lat = position.coords.latitude;
+				const long = position.coords.longitude;
+				fetch(`https://us1.locationiq.com/v1/reverse.php?key=${LOCATIONIQ_PRIVATE_TOKEN}&lat=${lat}&lon=${long}&format=json&addressdetails=1`)
+				.then(res => res.json())
+				.then(res => {
+					console.log(res);
+					if (res.hasOwnProperty('address') && res.address.hasOwnProperty('country_code')) {
+						return res.address.country_code.toUpperCase();
+					}
+					else return null;
+				})
+				.then(country_code => this.submitCoordinates(lat, long, country_code));
+			}
+		}
 
 		geolocator.getCurrentPosition(getPosition);
 	}
@@ -37,21 +108,21 @@ class App extends React.Component {
 	handleInput = (e) => {
 		let inputLocation = this.state.inputLocation;
 		this.setState({
-			location: inputLocation,
+			location: inputLocation
 		}, this.locationLookup)
 	}
 
 	locationLookup = () => {
-		fetch(`https://us1.locationiq.com/v1/search.php?key=${PRIVATE_TOKEN}&q=${this.state.location}&format=json`)
+		console.log("Looking up location...");
+
+		fetch(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_PRIVATE_TOKEN}&q=${this.state.location}&format=json&addressdetails=1`)
 		.then(res => res.json())
 		.then(res => {
 			console.log(res);
-			if (res.length >= 1){
-				let location = res[0];
-				this.setState({
-					locationLat: location.lat,
-					locationLong: location.lon
-				})
+			if (res.length >= 1) {
+				const location = res[0];
+				const country_code = (location.address.country_code) ? location.address.country_code.toUpperCase() : null;
+				this.submitCoordinates(location.lat, location.lon, country_code);
 			}
 			else {
 				this.setState({
@@ -62,15 +133,22 @@ class App extends React.Component {
 		})
 	}
 
+	submitCoordinates = (lat, long, country_code) => {
+		this.setState({
+			locationLat: lat,
+			locationLong: long,
+			locationCountry: country_code
+		})
+	}
+
   render() {
   	return (
 	    <div className="App">
-	      <h1>Should I Travel There (SITT)</h1>
+	      <h1>Should I Travel There</h1>
 	      <input type="text" value={this.state.inputLocation} onChange={this.handleInputChange} />
 				<button onClick={this.handleInput}> Submit </button>
 				<button onClick={this.getCurrentLocation}> Get Location </button>
-				<p>{(this.state.locationLat !== '') ? ((this.state.locationLat !== 'err') ? `${this.state.locationLat}, ${this.state.locationLong}` : 'Location not found, try again.') : null
-					}</p>
+				<UserConfig lat={this.state.locationLat} long={this.state.locationLong} country_code={this.state.locationCountry}/>
 	    </div>
 	  );
   }
