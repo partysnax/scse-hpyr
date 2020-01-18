765182f3544a4be365e40eb4da891fc86b3aa9c6
YuerZhu <45285516+YuerZhu@users.noreply.github.com>
2020-01-18 17:58:31 +0800

    jk

--- src/App.js
+++ src/App.js
@@ -481,14 +481,16 @@ class App extends React.Component {
 		        	<div className="text-muted">
 		        		<h4 className="subheader">real time travel recommendations!</h4>
 					</div>		        	
+		        </div>
+		        <div className="AppNonBanner">
+			    	<input className = "Input" type="text" Placeholder="Where do you plan to go?" value={this.state.inputLocation} onChange={this.handleInputChange} />
+					<div className = "buttonholder">
+						<button className = "button1" onClick={this.handleInput}> Submit </button>
+						<button className = "button1" onClick={this.getCurrentLocation}> Somewhere Nearby </button>
+					</div>
+					<UserConfig lat={this.state.locationLat} long={this.state.locationLong} countryCode={this.state.locationCountry} filterLocations={this.filterLocations}/>
+					<Results locationData={this.state.locationData}/>		        	
 		        </div>		       
-		    	<input className = "Input" type="text" Placeholder="Where do you plan to go?" value={this.state.inputLocation} onChange={this.handleInputChange} />
-				<div className = "buttonholder">
-					<button className = "button1" onClick={this.handleInput}> Submit </button>
-					<button className = "button1" onClick={this.getCurrentLocation}> Somewhere Nearby </button>
-				</div>
-				<UserConfig lat={this.state.locationLat} long={this.state.locationLong} countryCode={this.state.locationCountry} filterLocations={this.filterLocations}/>
-				<Results locationData={this.state.locationData}/>
 	        </div>
 	    );
    }
